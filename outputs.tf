247c2697918ebe95207b02fe74023ed1bf135523
Brent Gawryluik <brent.gawryluik@scalar.ca>
2017-11-14 21:46:53 -0800

    Added IAM output section

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,20 @@ output "bastion_dns_name" {
   description = "The Bastion's fully qualified domain name"
   value       = "${aws_route53_record.bastion.fqdn}"
 }
+
+# IAM
+output "bastion_role_arn" {
+  description = "The Bastion's EC2 IAM role ARN"
+  value       = "${aws_iam_role.bastion_role.arn}"
+}
+
+output "bastion_instance_profile_arn" {
+  description = "The Bastion's EC2 instance profile ARN"
+  value       = "${aws_iam_instance_profile.bastion_instance_profile.arn}"
+}
+
+output "bastion_instance_profile_name" {
+  description = "The Bastion's EC2 instance profile name"
+  value       = "${aws_iam_instance_profile.bastion_instance_profile.name}"
+}
+
